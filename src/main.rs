28bed2cabbcea90e0c5b88990080d3a9271a9d87
Ralf Speer <ralf.speer@hs-furtwangen.de>
2022-09-06 15:11:13 +0200

    Json manipulation done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use core::panic;
+
 use sqlite::{State, Statement};
 use regex::Regex;
+
+extern crate serde_json;
+use serde_json::Value as JsonValue;
+use serde_json::json;
 
 fn main() {
     find_and_delete_message();
 }
+
 
 fn find_and_delete_message(){
     let connection = sqlite::open("homeserver.db").unwrap();
@@ -26,19 +33,31 @@ fn find_and_delete_message(){
 
     let json = find_json(new);
 
-    println!("{}",json);
-
-    let re = Regex::new(r"Sup").unwrap();
-    assert!(re.is_match(&json));
-
     let result =  str::replace(&json, "Sup", "Delted Message"); 
     let mut update_statement: String = "UPDATE event_json  ".to_owned();
 
     update_statement.push_str("SET json ='");
     update_statement.push_str(&result);
     update_statement.push_str("' WHERE event_id = '$zhXyXUJvn7NwHtgzKpls_9uem_TsWm9rpCj6N3GO8eE';");
+    connection.prepare(&update_statement).unwrap();
+    
+   
+    find_message_content(result);
+}
 
-    connection.prepare(update_statement).unwrap();
+fn find_message_content(result: String){
+    let message_from_json:Result<JsonValue, serde_json::Error> = serde_json::from_str(&result);
+   
+    if message_from_json.is_ok(){
+        let mut p: JsonValue = message_from_json.unwrap();
+        println!("body is: {}", p["content"]["body"] );
+        p["content"]["body"] = json!("Delted Message");
+        println!("body is: {}", p["content"]["body"] );
+        println!("{}", p);
+    }else{
+        println!("Feierabend!");
+        panic!();
+    }
 }
 
 fn find_messages(mut statement: Statement) -> String{
