0c8cd1952bc7caac2df78d29c4255d574abc8a29
Ralf Speer <ralf.speer@hs-furtwangen.de>
2022-09-16 17:22:35 +0200

    added progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,99 @@
 use sqlite3::{State, Statement};
 extern crate serde_json;
-use serde_json::Value as JsonValue;
+use indicatif::ProgressBar;
 use serde_json::json;
-use std::io;
+use serde_json::Value as JsonValue;
 use std::env;
+use std::io;
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
-    if args.len() == 3{
-       let user = &args[1];
-       let path = &args[2];
-        
-       find_and_delete_message(user.to_string(),  path.to_string());
-      
+
+    if args.len() == 3 {
+        let user = &args[1];
+        let path = &args[2];
+
+        find_and_delete_message(user.to_string(), path.to_string());
     }
-    if args.len() != 0 && args.len() < 3{
+    if args.len() != 0 && args.len() < 3 {
         print!("NOT ENOUGH ARGUMENTS! \u{1F44E}");
-    }
-    else{
+    } else if args.len() == 0 {
+        let mut input = String::new();
 
-    let mut input = String::new();
-
-    println!("Input Username");
-    match io::stdin().read_line(&mut input){
-        Ok(_)=>{
-            println!("Deleting for User: {}", input.to_lowercase());
+        println!("Input Username");
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                println!("Deleting for User: {}", input.to_lowercase());
+            }
+            Err(e) => println!("Something went wrong {}", e),
         }
-        Err(e) => println!("Something went wrong {}", e),
-    }
 
-    let new_input = input.trim_end().to_lowercase();
-    find_and_delete_message(new_input, "homeserver.db".to_string());
+        let new_input = input.trim_end().to_lowercase();
+        find_and_delete_message(new_input, "homeserver.db".to_string());
     }
-
-    
 }
 
-
-fn find_and_delete_message(username: String, path_to_db: String){
+fn find_and_delete_message(username: String, path_to_db: String) {
     let connection = sqlite3::open(path_to_db).unwrap();
-    let mut event_id_statement = "select event_id from events where type='m.room.encrypted' and sender='@".to_owned();
+    let mut event_id_statement =
+        "select event_id from events where type='m.room.message' and sender='@".to_owned();
     event_id_statement.push_str(&username);
-    event_id_statement.push_str(":matrixtest.digitalmedcare.de'");
+    event_id_statement.push_str(":matrix.digitalmedcare.de'");
 
-    println!("event id statement: {}", event_id_statement);
-
-
-    let  statement = connection
-    .prepare(event_id_statement)
-    .unwrap();
+    let statement = connection.prepare(event_id_statement).unwrap();
 
     let messages = find_messages(statement);
-    let mut counter = 0;
-    for i in messages.iter(){
-        counter = counter + 1;
-        println!("{}: {}",counter, i);
-     
-    let mut where_statement = "select * from event_json where event_id = ".to_owned();
-    where_statement.push_str("'");
-    where_statement.push_str(&i);
-    where_statement.push_str("'");
-    let new =  connection
-        .prepare(where_statement)
-        .unwrap();
+
+    let bar = ProgressBar::new(messages.len().try_into().unwrap());
+
+    for i in messages.iter() {
+        bar.inc(1);
+        let mut where_statement = "select * from event_json where event_id = ".to_owned();
+        where_statement.push_str("'");
+        where_statement.push_str(&i);
+        where_statement.push_str("'");
+        let new = connection.prepare(where_statement).unwrap();
 
         let json = find_json(new);
         let mut update_statement: String = "UPDATE event_json  ".to_owned();
-        let new_json= find_message_content(json);
-    
-    
-        println!{"new_json {}",new_json};
-    
+        let new_json = find_message_content(json);
+
         update_statement.push_str("SET json ='");
         update_statement.push_str(&new_json);
         update_statement.push_str("' WHERE event_id = '");
-        update_statement.push_str( &i);
+        update_statement.push_str(&i);
         update_statement.push_str("';");
- 
-       connection.execute(&update_statement).unwrap();
-        
-        println!("{}", update_statement);
- }
 
+        connection.execute(&update_statement).unwrap();
+    }
+    bar.finish();
 }
 
-fn find_message_content(result: String) -> String{
-    
-    let message_from_json:Result<JsonValue, serde_json::Error> = serde_json::from_str(&result);
-    
-    if message_from_json.is_ok(){
+fn find_message_content(result: String) -> String {
+    let message_from_json: Result<JsonValue, serde_json::Error> = serde_json::from_str(&result);
+
+    if message_from_json.is_ok() {
         let mut p: JsonValue = message_from_json.unwrap();
         p["content"] = json!({
             "body": "MESSAGE DELETED",
             "msgtype": "m.text"});
-        //p["content"]["org.matrix.msc1767.text"] = json!("Deleted Message");
         return p.to_string();
-    }else{
+    } else {
         return "Feierabend".to_string();
-        }
-
-    
+    }
 }
 
-fn find_messages(mut statement: Statement) -> Vec<String>{ 
+fn find_messages(mut statement: Statement) -> Vec<String> {
     let mut messages_vec: Vec<String> = Vec::new();
-    while let State::Row = statement.next().unwrap(){
+    while let State::Row = statement.next().unwrap() {
         messages_vec.push(statement.read::<String>(0).unwrap().to_string());
     }
     return messages_vec;
 }
 
-fn find_json(mut statement: Statement) -> String{
-    while let State::Row = statement.next().unwrap(){
-        return  statement.read::<String>(3).unwrap().to_string();
+fn find_json(mut statement: Statement) -> String {
+    while let State::Row = statement.next().unwrap() {
+        return statement.read::<String>(3).unwrap().to_string();
     }
     return "failed".to_string();
 }
