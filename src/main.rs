c67a6d5d6501e1b4653cbeda22fd971c09b74960
Ralf Speer <ralf.speer@hs-furtwangen.de>
2022-10-10 17:14:09 +0200

    added prev_events vector

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::io;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let matrix = ":matrix.digitalmedcare.de"; 
+    let matrix = ":matrix.digitalmedcare.de";
     println!("MATRIX: {}", matrix);
 
     if args.len() == 3 {
@@ -24,17 +24,18 @@ fn main() {
         find_and_delete_message(user.to_string(), path.to_string(), matrix.to_string());
     }
     if args.len() != 1 && args.len() < 3 {
-        println!("length: {}",args.len());
+        println!("length: {}", args.len());
         println!("NOT ENOUGH ARGUMENTS! \u{1F44E}");
-    }
-
-    if args.len() == 1 {
+    } else if args.len() == 1 {
         user_input();
     }
 }
 
 fn find_and_delete_message(username: String, path_to_db: String, matrix_adress: String) {
     let connection = sqlite3::open(path_to_db).unwrap();
+
+    // !!!!! MESSAGE TYPE ALS PARAMETER HINZUFÜGEN!!!!
+
     let mut event_id_statement =
         "select event_id from events where type='m.room.encrypted' and sender='@".to_owned();
     event_id_statement.push_str(&username);
@@ -59,7 +60,9 @@ fn find_and_delete_message(username: String, path_to_db: String, matrix_adress: 
 
         let json = find_json(new);
         let mut update_statement: String = "UPDATE event_json  ".to_owned();
-        let new_json = find_message_content(json);
+        let new_json = find_message_content(&json);
+        let mut prev_events: Vec<String> = Vec::new();
+        prev_events.push(find_prev_events(json));
 
         update_statement.push_str("SET json ='");
         update_statement.push_str(&new_json);
@@ -72,7 +75,19 @@ fn find_and_delete_message(username: String, path_to_db: String, matrix_adress: 
     bar.finish();
 }
 
-fn find_message_content(result: String) -> String {
+fn find_prev_events(result: String) -> String{
+        let message_from_json: Result<JsonValue, serde_json::Error> = serde_json::from_str(&result);
+    
+        if message_from_json.is_ok() {
+            let p: JsonValue = message_from_json.unwrap();
+            println!("prev Event: {}", p["prev_events"]);
+            return p["prev_events"].to_string();
+        } else {
+            return "Feierabend".to_string();
+        }
+}
+
+fn find_message_content(result: &String) -> String {
     let message_from_json: Result<JsonValue, serde_json::Error> = serde_json::from_str(&result);
 
     if message_from_json.is_ok() {
@@ -101,42 +116,40 @@ fn find_json(mut statement: Statement) -> String {
     return "failed".to_string();
 }
 
-fn user_input(){
+fn user_input() {
     let mut input1 = String::new();
-        let mut input2 = String::new();
-        let mut input3 = String::new();
-        
-        println!("Input Username");
-        match io::stdin().read_line(&mut input1) {
-            Ok(_) => {
-                println!("Deleting for User: {}", input1.to_lowercase());
-            }
-            Err(e) => println!("Something went wrong {}", e),
+    let mut input2 = String::new();
+    let mut input3 = String::new();
+
+    println!("Input Username");
+    match io::stdin().read_line(&mut input1) {
+        Ok(_) => {
+            println!("Deleting for User: {}", input1.to_lowercase());
         }
+        Err(e) => println!("Something went wrong {}", e),
+    }
 
-        let username = input1.trim_end().to_lowercase();
+    let username = input1.trim_end().to_lowercase();
 
-        println!("path to homeserver.db");
-        match io::stdin().read_line(&mut input2) {
-            Ok(_) => {
-                println!("Deleting for User: {}", input2.to_lowercase());
-            }
-            Err(e) => println!("Something went wrong {}", e),
+    println!("path to homeserver.db");
+    match io::stdin().read_line(&mut input2) {
+        Ok(_) => {
+            println!("Deleting for User: {}", input2.to_lowercase());
         }
+        Err(e) => println!("Something went wrong {}", e),
+    }
 
-        let homserver = input2.trim_end().to_lowercase();
+    let homserver = input2.trim_end().to_lowercase();
 
-        
-
-        println!("matrix adress");
-        match io::stdin().read_line(&mut input3) {
-            Ok(_) => {
-                println!("Deleting for User: {}", input3.to_lowercase());
-            }
-            Err(e) => println!("Something went wrong {}", e),
+    println!("matrix adress");
+    match io::stdin().read_line(&mut input3) {
+        Ok(_) => {
+            println!("Deleting for User: {}", input3.to_lowercase());
         }
+        Err(e) => println!("Something went wrong {}", e),
+    }
 
-        let matrix = input3.trim_end().to_lowercase();
+    let matrix = input3.trim_end().to_lowercase();
 
-        find_and_delete_message(username, homserver, matrix);
+    find_and_delete_message(username, homserver, matrix);
 }
