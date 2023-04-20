908660c8ca89c04d4da316c1fad547011af03fde
Goukart <benjaminherzberger@web.de>
2023-04-20 08:46:27 +0200

    update user deletion
    add some sql queries to delete more of the user

---
+++ api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+import mtd
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {'API_root': 'Default response to [localhost:8000]', 'data': 0}
+
+
+@app.get('/test_api/{parameter}')
+async def test_api(parameter: str):
+    return {"function_name": test_api.__name__, 'parameter': parameter, 'return_value': mtd.api_test(parameter)}
+
+
+@app.get('/test_token/{parameter}')
+async def test_token(token: str):
+    return {"function_name": test_token.__name__, 'token': token, 'valid': mtd.api_test(token)}
+
+
+@app.get('/delete_user/{username}')
+async def delete_user(username: str):
+    return {delete_user.__name__: 'Deleting user', 'user': username, 'call': mtd.api_test(username)}
--- mtd.py
+++ mtd.py
@@ -4,7 +4,15 @@ import json
 import sqlite3
 import sys
 from os.path import exists
+import time
 
+# purge time
+from typing import Any, Set, Tuple, cast
+import logging
+
+# from typing import Any, List, Set, Tuple, cast
+
+logger = logging.getLogger(__name__)
 
 # Global object to execute sql queries
 db_cursor: sqlite3.Cursor
@@ -14,6 +22,21 @@ server: str
 # ToDo keep track of what ids are deleted and search dump to make sure
 all_collected: list
 
+# ToDo checksum compare before and after user deletion (only changes are by user)
+# ToDo search every event iy by user and user name in sql dump
+
+
+# check if room is created by user then, delete first msg matrix way
+
+def api_test(param: str):
+    print("API call")
+    return "API does works: " + param
+
+
+def valid_token(token: str) -> bool:
+    user_id: str = select_from("access_tokens", "user_id", f"token={token}")[0]
+    return bool(select_from("users", "admin", f"user_id={user_id}")[0])
+
 
 def select_from(_table: str, _column: str, _condition: str) -> list:
     global db_cursor
@@ -22,9 +45,15 @@ def select_from(_table: str, _column: str, _condition: str) -> list:
     sql_query: str = f"SELECT {_column} FROM {_table} WHERE {_condition};"
 
     # Execute sql query on database file
+    # ToDo what is this?
+    #db_cursor.execute("SELECT event_id FROM events WHERE \"type\" in ('m.room.message', 'm.room.encrypted') AND sender='@bobby:matrix.digitalmedcare.de';")
     db_cursor.execute(sql_query)
+
     # Collect the results table, this is a list of all messages from _user of given type
     result_list: list = db_cursor.fetchall()
+
+    # Todo Debug
+    #print(result_list)
 
     return result_list
 
@@ -33,10 +62,10 @@ def select_message_ids_from(_user: str) -> list:
     global db_cursor
     global server
 
-    message_type = "m.room.message"  # ToDo what type is really needed?
+    message_types = "('m.room.message', 'm.room.encrypted')"  # ToDo what type is really needed?
 
     # Select all messages from _user or type message_type
-    return select_from("events", "event_id", f"type='{message_type}' AND sender='@{_user}:{server}'")
+    return select_from("events", "event_id", f"\"type\" in {message_types} AND sender='@{_user}:{server}'")
 
 
 # ToDo this might be obsolete
@@ -76,16 +105,36 @@ def setup_database(_database: str) -> sqlite3.Connection:
     return db_connection
 
 
-def delete_room_events(_user: str):
-    user_id = f"@{_user}:{server}"
+def delete_room_events(_user_id: str):
+    print("\n\nDeleting user events")
 
-    print(select_from("room_memberships", "event_id, room_id", f"user_id='{user_id}'"))
+    for event_id in select_from("room_memberships", "event_id", f"sender='{_user_id}'"):
+        delete_event(event_id)
+    #db_cursor.execute(f"DELETE FROM room_memberships WHERE sender='{_user_id}';")
+
+    #print(select_from("room_memberships", "event_id, room_id", f"sender='{_user_id}'"))
+
+
+def delete_event(_event_id: str):
+    db_cursor.execute(f"DELETE FROM event_json WHERE event_id='{_event_id}';")
+    db_cursor.execute(f"DELETE FROM current_state_events WHERE event_id='{_event_id}';")
+    db_cursor.execute(f"DELETE FROM event_to_state_groups WHERE event_id='{_event_id}';")
+    db_cursor.execute(f"DELETE FROM stream_ordering_to_exterm WHERE event_id='{_event_id}';")
+    db_cursor.execute(f"DELETE FROM event_auth WHERE event_id='{_event_id}';")
+    db_cursor.execute(f"DELETE FROM event_auth_chains WHERE event_id='{_event_id}';")
+    db_cursor.execute(f"DELETE FROM event_edges WHERE event_id='{_event_id}';")
+    db_cursor.execute(f"DELETE FROM state_groups WHERE event_id='{_event_id}';")
+    db_cursor.execute(f"DELETE FROM state_groups_state WHERE event_id='{_event_id}';")
+    db_cursor.execute(f"DELETE FROM event_json WHERE event_id='{_event_id}';")
 
 
 def delete_user(user: str) -> None:
     global server
     user_id: str = f"@{user}:{server}"
 
+    message_id_list: list = select_message_ids_from(user)
+
+    # From a github discussion
     db_cursor.execute(f"DELETE FROM users WHERE name='{user_id}';")
     db_cursor.execute(f"DELETE FROM user_directory WHERE user_id='{user_id}';")
     db_cursor.execute(f"DELETE FROM account_data WHERE user_id='{user_id}';")
@@ -93,6 +142,7 @@ def delete_user(user: str) -> None:
     db_cursor.execute(f"DELETE FROM user_external_ids WHERE user_id='{user_id}';")
     db_cursor.execute(f"DELETE FROM user_external_ids WHERE user_id='{user}';")
 
+    # Experimental
     db_cursor.execute(f"DELETE FROM user_ips WHERE user_id='{user_id}';")
     db_cursor.execute(f"DELETE FROM presence_stream WHERE user_id='{user_id}';")
     db_cursor.execute(f"DELETE FROM device_lists_stream WHERE user_id='{user_id}';")
@@ -107,14 +157,26 @@ def delete_user(user: str) -> None:
     db_cursor.execute(f"DELETE FROM ui_auth_sessions_credentials WHERE result='{user_id}';")
     db_cursor.execute(f"DELETE FROM access_tokens WHERE user_id='{user_id}';")
     db_cursor.execute(f"DELETE FROM e2e_one_time_keys_json WHERE user_id='{user_id}';")
+    db_cursor.execute(f"DELETE FROM current_state_delta_stream WHERE state_key='{user_id}';")
+    db_cursor.execute(f"DELETE FROM e2e_room_keys WHERE user_id='{user_id}';")
 
+    # Room events
+    db_cursor.execute(f"DELETE FROM state_events WHERE state_key='{user_id}';")
+
+    # Other?
     db_cursor.execute(f"DELETE FROM events WHERE sender='{user_id}';")
-    #db_cursor.execute(f"DELETE FROM event_json WHERE sender='{user_id}';")
-
-    message_id_list: list = select_message_ids_from(user)
+    # db_cursor.execute(f"DELETE FROM event_json WHERE sender='{user_id}';")
 
     for message_id in message_id_list:
         delete_message(message_id)
+
+    with open('del_test.sql', 'r') as f:
+        read_data = f.read()
+    for id in message_id_list:
+        if id in read_data:
+            print("True, ", id)
+
+    delete_room_events(user_id)
 
 
 def delete_message(id: str) -> None:
@@ -141,6 +203,7 @@ def main():
                         help='The name of the user to be deleted')
 
     # Optional argument, database path
+    # ToDo check for default paths, then prompt? maybe only need db file?
     parser.add_argument('config', type=str,
                         help='To get all necessary information like server domain and signing key you have to provide '
                              'the .yaml file.')
@@ -148,14 +211,18 @@ def main():
     # Parse arguments
     args = parser.parse_args()
 
+    # Measure run time
+    start_time = time.time()
+
     # Read the YAML file
     with open(args.config, "r") as stream:
         try:
             config: dict = yaml.safe_load(stream)
+            # ToDo hoe do they go about? Also hard coded structure?
             _db: str = config['database']['args']['database']
             _server: str = config['server_name']
-            print(_db)
-            print(_server)
+            #print(_db)
+            #print(_server)
         except yaml.YAMLError as exc:
             print(exc)
 
@@ -165,14 +232,22 @@ def main():
     server = _server
 
     # Main body
-    # delete_user(args.username)
-    delete_room_events(args.username)
+    delete_user(args.username)
+    # _purge_history_txn("!OuwzcoSSRFQqnjXlYK:matrix.digitalmedcare.de", "syt_YWRtaW4_WSzXELBHTWvOkzZeWxtN_3GkVMI", )
 
     # Write changes to database
     db_connection.commit()
 
+    # Print info
+    print(f"Deleting [{args.username}] from [{_db}] at [{_server}]")
+
     # Close the connection
     db_connection.close()
+
+    # Measure run time
+    execution_time = (time.time() - start_time)
+    print('Execution time in seconds: ' + str(execution_time))
+# ToDo performance measurement and counting in debug mode (also count outside of performance test)
 
 
 if __name__ == "__main__":
